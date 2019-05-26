f24b89e9ad3d17a161519ca28b3370e21ebc7cba
hesichuan <364079154@qq.com>
2019-05-26 19:25:39 +0800

    finish detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,7 +2,9 @@
   <div class="detail">
     <DetailBanner/>
     <DetailHeader/>
-    <div class="content"></div>
+    <div class="content">
+      <DetailList :list="list" />
+    </div>
   </div>
 </template>
 
@@ -10,13 +12,32 @@
 import { Component, Vue } from 'vue-property-decorator'
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
 @Component({
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 })
 export default class Detail extends Vue {
+  private list: Array<object> = [
+    {
+      title: '成人票',
+      children: [
+        {
+          title: '成人三馆联票',
+          children: [
+            { title: '成人三馆联票 - 长安街连锁店销售' }
+          ]
+        },
+        { title: '成人五馆联票' }
+      ]
+    },
+    { title: '学生票' },
+    { title: '儿童票' },
+    { title: '特惠票' }
+  ]
 }
 </script>
 
